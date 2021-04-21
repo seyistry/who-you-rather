3717cfb4a73b3417bd9de6ab60edb42dbe61dc44
seyi egunjobi <seyistry@gmail.com>
2021-04-21 19:06:36 +0100

    add reducers to the redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
 import App from "./components/App";
+
+const store = createStore(reducers)
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );

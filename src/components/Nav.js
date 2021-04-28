a92e8be8b55eb77ee788eb9cc12629135b21e9d8
seyi egunjobi <seyistry@gmail.com>
2021-04-28 14:13:07 +0100

    Nav now includes UsersName img and Logout button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-
-export default function Nav () {
-    return (
-        <nav className="nav">
-            <ul>
-                <li>
-                    <NavLink to="/" exact activeClassName="active">
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/add" exact activeClassName="active">
-                        New Question
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/champs" exact activeClassName="active">
-                        Leaderboard
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
+import { connect } from "react-redux";
+class Nav extends Component {
+    handleClick = (e) => {
+        e.preventDefault();
+        this.setState({ toggle: false });
+    };
+    render() {
+        const { authedUser, users } = this.props;
+        return (
+            <nav className="nav">
+                <ul>
+                    <li>
+                        <NavLink to="/" exact activeClassName="active">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/add" exact activeClassName="active">
+                            New Question
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/champs" exact activeClassName="active">
+                            Leaderboard
+                        </NavLink>
+                    </li>
+                    <li>
+                        {authedUser === null ? (
+                            <div></div>
+                        ) : (
+                            <div>
+                                <span>Hello {authedUser}</span>
+                                <img
+                                    src={
+                                        users[authedUser]
+                                            .avatarURL
+                                    }
+                                />
+                                <span onClick={this.handleClick}>Logout</span>
+                            </div>
+                        )}
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
 }
+
+function mapStateToProps({ authedUser, users }) {
+    console.log(users);
+    return {
+        authedUser,
+        users,
+    };
+}
+
+export default connect(mapStateToProps)(Nav);
